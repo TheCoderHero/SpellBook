e1ed66d915e7ab01adb924eeda2780dd9f018d5b
John Tanner <thecoderhero@gmail.com>
2019-08-12 21:58:42 -0700

    Added Bootstrap elements and CSS.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let lock = document.querySelector('button');
 
 lock.addEventListener('click', function() {
-    window.location.href='https://thecoderhero.github.io/SpellBook/login.html';
+    window.location.href='login.html';
 })
